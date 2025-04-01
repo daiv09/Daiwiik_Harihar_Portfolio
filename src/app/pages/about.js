0563a2fead877174c793165d002e8ad117cd7422
Daiwiik Harihar <daiwiikharihar17147@gmail.com>
2025-04-01 03:16:12 +0200

    Made changesto pages.tsx

---
+++ src/app/pages/about.js
@@ -0,0 +1,3 @@
+export default function About() {
+    return <h1 className="text-center text-3xl font-bold mt-10">About Me</h1>;
+  }
--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import {useState} from "react";
 
 const Navbar = () => {
@@ -10,7 +11,7 @@ const Navbar = () => {
     <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         {/* Logo */}
-        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <Link href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <Image
             src="https://flowbite.com/docs/images/logo.svg"
             className="h-8"
@@ -21,7 +22,7 @@ const Navbar = () => {
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             MyPortfolio
           </span>
-        </a>
+        </Link>
 
         {/* Mobile Menu Button */}
         <button
@@ -54,13 +55,13 @@ const Navbar = () => {
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             {/* Home */}
             <li>
-              <a
+              <Link
                 href="#"
                 className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                 aria-current="page"
               >
                 Home
-              </a>
+              </Link>
             </li>
 
             {/* About Me Dropdown */}
@@ -88,80 +89,80 @@ const Navbar = () => {
               </button>
               <ul className="absolute left-0 hidden group-hover:block mt-2 w-44 bg-white rounded-lg shadow-lg dark:bg-gray-700">
                 <li>
-                  <a
-                    href="#about"
+                  <Link
+                    href="/pages/about"
                     className="block px-4 py-2 text-gray-700 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-600"
                   >
                     About Me
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a
+                  <Link
                     href="#resume"
                     className="block px-4 py-2 text-gray-700 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-600"
                   >
                     Resume
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a
+                  <Link
                     href="#testimonials"
                     className="block px-4 py-2 text-gray-700 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-600"
                   >
                     Testimonials
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </li>
 
             {/* Projects */}
             <li>
-              <a
+              <Link
                 href="#projects"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500"
               >
                 Projects
-              </a>
+              </Link>
             </li>
 
             {/* Skills */}
             <li>
-              <a
+              <Link
                 href="#skills"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500"
               >
                 Skills
-              </a>
+              </Link>
             </li>
 
             {/* Blog */}
             <li>
-              <a
+              <Link
                 href="#blog"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500"
               >
                 Blog
-              </a>
+              </Link>
             </li>
 
             {/* Services */}
             <li>
-              <a
+              <Link
                 href="#services"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500"
               >
                 Services
-              </a>
+              </Link>
             </li>
 
             {/* Contact Me */}
             <li>
-              <a
+              <Link
                 href="#contact"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500"
               >
                 Contact Me
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
